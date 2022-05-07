5c0d4266bcfa4cffa6c0a84e173c34370d13b7c1
hermani456 <hermani457@gmail.com>
2022-05-07 11:19:30 -0400

    update

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ http
 		const urlParse = url.parse(req.url, true)
 		if (urlParse.pathname === '/') {
 			res.writeHead(200, { 'Content-Type': 'text/html' })
-			fs.readFile('index.html', 'utf8', (_err, html) => {
-				res.write(html)
-				res.end()
+			fs.readFile('index.html', 'utf8', (err, html) => {
+				if (err) res.end('html file not found')
+				else res.end(html)
 			})
 		}
 		if (urlParse.pathname === '/style') {
 			res.writeHead(200, { 'Content-Type': 'text/css' })
-			fs.readFile('style.css', (_err, css) => {
-				res.write(css)
-				res.end()
+			fs.readFile('style.css', (err, css) => {
+				if(err) res.end('css file not found')
+				else res.end(css)
 			})
 		}
 		if (urlParse.pathname.includes('/img')) {
